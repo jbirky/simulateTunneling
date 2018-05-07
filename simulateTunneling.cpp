5c34613cb23f846aac58fde604e7118e41982b9d
jbirky <jbirky@ucsd.edu>
2018-05-07 15:48:39 -0700

    general potential

--- simulateTunneling.cpp
+++ simulateTunneling.cpp
@@ -2,27 +2,19 @@
 // JESSICA BIRKY (A13002163)
 // ===================================
 
-#include <iostream>
-#include <fstream>
-#include <complex>
-#include <string>
-#include <vector>
-#include <math.h>
-#include <ctime>
-#include <cblas.h>
+#include "setup.cpp"
 #include "utils.cpp"
-#include "pathIntegral.cpp"
 using namespace std;
-
-typedef complex<double> cdouble;
-typedef vector<cdouble> matrix;
 
 
 // ===================================
 // DECLARE FUNCTIONS
 // ===================================
 
-// From pathIntegral.cpp
+double 	x(int i);
+double 	potential(double x);
+
+// Path Integral Functions
 matrix 			returnKep();
 matrix 			returnPropagator();							// return propagator matrix at time step N
 void			saveWaveFunctions();						// save wave functions, probability amplitudes, and expected values at each time step
@@ -54,7 +46,7 @@ int main()
 
 	// ========================================= 
 
-
+	cout<<D<<endl;
 
 	// =========================================
 
@@ -70,9 +62,9 @@ int main()
 // DEFINE POTENTIAL AND INITIAL CONDITION
 // ==========================================
 
-double x(int d)
+double x(int i)
 {
-	return X0 + d*DEL_X;
+	return X0 + i*DEL_X;
 }
 
 
@@ -88,9 +80,9 @@ vector<cdouble> phiInit()
 }
 
 
-vector<cdouble> potential()
+double potential(double x)
 {
-	vector<cdouble> V(D,0);
+	double V = ALPHA * pow(x, 4) + BETA * pow(x, 2) + pow(BETA, 2) / (4*ALPHA);
 
 	return V;
 }
@@ -99,3 +91,192 @@ vector<cdouble> potential()
 // ===================================
 // MONTE CARLO 
 // ===================================
+
+
+
+// ===================================
+// CREATE PROPAGATOR MATRIX
+// ===================================
+
+
+matrix returnKep()
+{
+	matrix Kep(D*D,0);
+
+	// cdouble A(sqrt(PI * h * DEL_T), sqrt(PI * h * DEL_T)); 
+	double A = 1; // normalize wave function later
+	cdouble I(0,1);
+	cdouble efactor;
+
+	// Generate elementary Kep matrix
+	for (int i=0; i<D; i++) {
+		for (int j=0; j<D; j++) {
+
+			efactor = I*DEL_T/h * (.5*m*pow((x(j)-x(i))/DEL_T, 2) - potential(pow(((x(j)+x(i))/2.), 2)));
+			// cout<<exp(efactor)<<endl;
+
+			Kep[i*D + j] = exp(efactor) / A;
+		}
+	}
+
+	return Kep;
+}
+
+
+matrix returnPropagator()
+{
+	// Return the Nth step propagator matrix
+
+	matrix Kep = returnKep();
+	matrix K = Kep;
+
+	// Multiply to the nth power to propagate by n time steps
+	for (int n=1; n<N; n++) {
+		K = matrixMatrixMultiply(K, Kep);
+	}
+
+	// Multiply by step size factors
+	double factor = pow(DEL_X, N-1);// * DEL_T;
+
+	for (int i=0; i<K.size(); i++) {
+		K[i] *= factor;
+	}
+
+	return K;
+}
+
+
+// ===================================
+// COMPUTE WAVE FUNCTION VECTOR
+// ===================================
+
+vector<cdouble> normalizeWF(vector<cdouble> wf)
+{
+
+	vector<cdouble> norm_wf(D,0);
+	double sum = 0;
+
+	// Square wave function: multiply by complex conjugate
+	for (int i=0; i<D; i++) {
+		sum += (wf[i] * conj(wf[i])).real() * DEL_X;
+	}
+
+	// Divide by normalization factor such that sum(phi*phi) = 1
+	for (int i=0; i<D; i++) {
+		norm_wf[i] = wf[i] / sqrt(sum);
+	}
+
+	return norm_wf;
+}
+
+
+vector<double> returnProbability(vector<cdouble> wf) 
+{
+	vector<double> wf_sq(D,0);
+
+	for (int i=0; i<D; i++) {
+		wf_sq[i] = (conj(wf[i]) * wf[i]).real();
+	}
+
+	return wf_sq;
+}
+
+
+void saveWaveFunctions()
+{
+	vector<cdouble> wf_n;
+	vector<cdouble> wf_n_1;
+	vector<cdouble> wf_norm;
+	vector<double> wf_sq;
+	string sname0;
+	string sname1;
+	vector<double> avg_pos(P+1,0);
+	vector<double> avg_eng(P+1,0);
+	vector<double> avg_kin(P+1,0);
+	vector<double> avg_pot(P+1,0);
+
+	matrix K = returnPropagator();
+
+	for (int i=0; i<P+1; i++) {
+
+		if (i == 0) {
+			wf_n = phiInit();
+			wf_n_1 = wf_n;
+		} else {
+			wf_n = matrixVectorMultiply(K, wf_n_1);
+			wf_n_1 = wf_n;
+		}
+
+		// Save wave function vectors
+		wf_norm = normalizeWF(wf_n);
+		sname0 = "wave_func/phi" + to_string(i) + ".dat";
+		saveFileC(wf_norm, sname0);
+
+		// Save probability amplitudes
+		wf_sq = returnProbability(wf_norm);
+		sname1 = "wave_prob/phi_sq" + to_string(i) + ".dat";
+		saveFileR(wf_sq, sname1);
+
+		// Compute average values
+		avg_pos[i] = avgPosition(wf_norm);
+		avg_pot[i] = avgPotential(wf_norm);
+		avg_kin[i] = avgKinetic(wf_norm);
+		avg_eng[i] = avg_pot[i] + avg_kin[i];
+	}
+
+	// Save average values
+	string sname2 = "expected/avg_pos.dat";
+	string sname3 = "expected/avg_pot.dat";
+	string sname4 = "expected/avg_kin.dat";
+	string sname5 = "expected/avg_eng.dat";
+	saveFileR(avg_pos, sname2);
+	saveFileR(avg_pot, sname3);
+	saveFileR(avg_kin, sname4);
+	saveFileR(avg_eng, sname5);
+}
+
+
+// ===================================
+// EXPECTED VALUES
+// ===================================
+
+double avgPosition(vector<cdouble> wf)
+{
+	double avg_pos = 0;
+	
+	for (int i=0; i<D; i++) {
+		avg_pos += (conj(wf[i]) * x(i) * wf[i]).real() * DEL_X;
+	}
+
+	return avg_pos;
+}
+
+
+double avgKinetic(vector<cdouble> wf)
+{
+	double avg_kin = 0;
+
+	for (int i=0; i<D; i++) {
+		if (i == 0) {
+			avg_kin += (-h*h / (2*m)) * (conj(wf[i]) * (wf[i+1] - 2.*wf[i])).real() / DEL_X;
+		} else if (i == D) {
+			avg_kin += (-h*h / (2*m)) * (conj(wf[i]) * (- 2.*wf[i] + wf[i-1])).real() / DEL_X;
+		} else {
+			avg_kin += (-h*h / (2*m)) * (conj(wf[i]) * (wf[i+1] - 2.*wf[i] + wf[i-1])).real() / DEL_X;
+		}
+	}
+
+	return avg_kin;
+}
+
+
+double avgPotential(vector<cdouble> wf)
+{
+	double avg_pot = 0;
+
+	for (int i=0; i<D; i++) {
+		avg_pot += (conj(wf[i]) * potential(x(i)) * wf[i]).real() * DEL_X;
+	}
+
+	return avg_pot;
+}
